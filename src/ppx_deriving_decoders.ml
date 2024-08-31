2b94994cfd7d06cbc2000d6d79393db4196c2f6e
Ben Bellick <benbellick@protonmail.com>
2024-08-31 16:13:28 -0500

    basic structure

--- src/ppx_deriving_decoders.ml
+++ src/ppx_deriving_decoders.ml
@@ -1,40 +1,17 @@
 open Ppxlib
 
-
-(* Generate a module name Info_t from type [t] *)
-let module_name_of_type t : string option loc =
-  let type_name = t.ptype_name.txt in
-  { t.ptype_name with txt = Some( "Info_" ^ type_name)  }
-
-let str_gen ~loc ~path (_rec, t) =
-  (* All nodes created using this Ast module will use [loc] by default *)
-  let (module Ast) = Ast_builder.make loc in
-  (* we are silently dropping mutually recursive definitions to keep things
-     brief *)
+let str_gen ~(loc : location) ~(path : label)
+    ((_rec : rec_flag), (t : type_declaration list)) : structure_item list =
+  let _loc = loc in
+  let _path = path in
   let t = List.hd t in
-  let info_module =
-    let expr =
-      (* we are using this ppxlib function to generate a full name for the type
-         that includes the type variable *)
-      let name =
-        core_type_of_type_declaration t
-        |> string_of_core_type
-      in
-      Ast.pmod_structure (
-        [%str
-          let path = [%e Ast.estring path]
-          let name = [%e Ast.estring name]
-        ])
-    in
-    let name = module_name_of_type t in
-    Ast.module_binding ~name ~expr
-    |> Ast.pstr_module
-  in
-  [info_module]
-
+  match t.ptype_kind with
+  | Ptype_abstract -> []
+  | Ptype_variant _ -> []
+  | Ptype_record _ -> []
+  | Ptype_open -> []
 
 (* let str_gen ~loc ~path (_rec, (t : type_declaration)) = *)
-  
 
 (* let sig_gen ~loc ~path:_ (_rec, t) = *)
 (*   let (module Ast) = Ast_builder.make loc in *)
@@ -60,5 +37,4 @@ let name = "decoders"
 let () =
   let str_type_decl = Deriving.Generator.make_noarg str_gen in
   (* let sig_type_decl = Deriving.Generator.make_noarg sig_gen in *)
-  Deriving.add name ~str_type_decl (* ~sig_type_decl *)
-  |> Deriving.ignore
+  Deriving.add name ~str_type_decl (* ~sig_type_decl *) |> Deriving.ignore
