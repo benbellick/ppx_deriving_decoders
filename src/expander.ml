886c0361a0e01d22daaa9442c45d757e5b941b78
Ben Bellick <benbellick@protonmail.com>
2024-09-18 20:41:28 -0600

    Explicitly substitute expressions for types

--- src/expander.ml
+++ src/expander.ml
@@ -24,8 +24,8 @@ let lident_of_constructor_decl (cd : constructor_declaration) =
   let name = cd.pcd_name.txt in
   Ast_builder.Default.Located.lident ~loc name (* Convert to lident *)
 
-let rec expr_of_typ (typ : core_type) ~(true_recurse : core_type list) :
-    expression =
+let rec expr_of_typ (typ : core_type)
+    ~(substitutions : (core_type * expression) list) : expression =
   let loc = { typ.ptyp_loc with loc_ghost = true } in
   match typ with
   | [%type: unit] | [%type: unit] -> Ast_builder.Default.evar ~loc "D.null"
@@ -47,39 +47,37 @@ let rec expr_of_typ (typ : core_type) ~(true_recurse : core_type list) :
       failwith "Cannot handle Bytes" (* TODO: figure out strategy *)
   | [%type: [%t? inner_typ] list] ->
       let list_decoder = Ast_builder.Default.evar ~loc "D.list" in
-      let sub_expr = expr_of_typ ~true_recurse inner_typ in
+      let sub_expr = expr_of_typ ~substitutions inner_typ in
       Ast_helper.Exp.apply ~loc list_decoder [ (Nolabel, sub_expr) ]
   | [%type: [%t? inner_typ] array] ->
       let array_decoder = Ast_builder.Default.evar ~loc "D.array" in
-      let sub_expr = expr_of_typ ~true_recurse inner_typ in
+      let sub_expr = expr_of_typ ~substitutions inner_typ in
       Ast_helper.Exp.apply ~loc array_decoder [ (Nolabel, sub_expr) ]
   | [%type: [%t? inner_typ] option] ->
       let opt_decoder = Ast_builder.Default.evar ~loc "D.nullable" in
-      let sub_expr = expr_of_typ ~true_recurse inner_typ in
+      let sub_expr = expr_of_typ ~substitutions inner_typ in
       Ast_helper.Exp.apply ~loc opt_decoder [ (Nolabel, sub_expr) ]
-  | { ptyp_desc = Ptyp_tuple typs; _ } -> expr_of_tuple ~true_recurse ~loc typs
+  | { ptyp_desc = Ptyp_tuple typs; _ } -> expr_of_tuple ~substitutions ~loc typs
   (* | { ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc; _ } -> _ *)
   (* | { ptyp_desc = Ptyp_alias _; _ } -> *)
   (*     failwith *)
   (*       (Format.sprintf "This alias was a failure...: %s\n" *)
   (*          (string_of_core_type typ)) *)
   | { ptyp_desc = Ptyp_constr ({ txt = Lident lid; _ }, []); _ } as other_type
-    ->
+    -> (
       (* In the case where our type is truly recursive, we need to instead do `type_aux ()` *)
       let eq (ct1 : core_type) (ct2 : core_type) =
         (* TODO: This is a terrible way to compare the types... *)
         string_of_core_type ct1 = string_of_core_type ct2
       in
-      if CCList.mem ~eq other_type true_recurse then
-        let open Ast_builder.Default in
-        let aux_fn_e = evar ~loc (lid ^ "_decoder_aux") in
-        [%expr [%e aux_fn_e]]
-      else Ast_builder.Default.evar ~loc (to_decoder_name lid)
+      match CCList.assoc_opt ~eq other_type substitutions with
+      | Some replacement -> replacement
+      | None -> Ast_builder.Default.evar ~loc (to_decoder_name lid))
   | _ ->
       Location.raise_errorf ~loc "Cannot construct decoder for %s"
         (string_of_core_type typ)
 
-and expr_of_tuple ~loc ~true_recurse ?lift typs =
+and expr_of_tuple ~loc ~substitutions ?lift typs =
   (* To help understand what this function is doing, imagine we had
      a type [type t = int * string * bool]. Then this will render the decoder:
      let t_decoder : t D.decoder =
@@ -98,7 +96,7 @@ and expr_of_tuple ~loc ~true_recurse ?lift typs =
      bool >>=:: fun arg3 -> succeed (lift (arg1, arg2, arg3))
   *)
   let argn = Printf.sprintf "arg%d" in
-  let typ_decoder_exprs = List.map (expr_of_typ ~true_recurse) typs in
+  let typ_decoder_exprs = List.map (expr_of_typ ~substitutions) typs in
   let base =
     (* Consists of the initial setup partial function def, which is the inport and local definition,
        as well as a running count of how many arguments there are *)
@@ -136,7 +134,7 @@ and expr_of_tuple ~loc ~true_recurse ?lift typs =
       complete_partial_expr [%expr succeed [%e var_tuple_lift]]
   | None -> complete_partial_expr [%expr succeed [%e var_tuple]]
 
-and expr_of_constr_decl ~true_recurse
+and expr_of_constr_decl ~substitutions
     ({ pcd_args; pcd_loc = loc; _ } as cstr_decl : constructor_declaration) =
   (* We assume at this point that the decomposition into indiviaul fields is handled by caller *)
   if pcd_args = Pcstr_tuple [] then
@@ -145,16 +143,16 @@ and expr_of_constr_decl ~true_recurse
     [%expr succeed [%e cstr]]
   else
     let cstr = lident_of_constructor_decl cstr_decl in
-    let sub_expr = expr_of_constr_arg ~true_recurse ~loc ~cstr pcd_args in
+    let sub_expr = expr_of_constr_arg ~substitutions ~loc ~cstr pcd_args in
     sub_expr
 
-and expr_of_constr_arg ~loc ~cstr ~true_recurse (arg : constructor_arguments) =
+and expr_of_constr_arg ~loc ~cstr ~substitutions (arg : constructor_arguments) =
   match arg with
-  | Pcstr_tuple tups -> expr_of_tuple ~true_recurse ~lift:cstr ~loc tups
+  | Pcstr_tuple tups -> expr_of_tuple ~substitutions ~lift:cstr ~loc tups
   | Pcstr_record labl_decls ->
-      expr_of_record ~loc ~true_recurse ~lift:cstr labl_decls
+      expr_of_record ~loc ~substitutions ~lift:cstr labl_decls
 
-and expr_of_record ~loc ~true_recurse ?lift label_decls =
+and expr_of_record ~loc ~substitutions ?lift label_decls =
   (* To help understand what this function is doing, imagine we had
      a type [type t = {i : int; s : string}]. Then this will render the decoder:
      let t_decoder : t D.decoder =
@@ -183,7 +181,7 @@ and expr_of_record ~loc ~true_recurse ?lift label_decls =
   in
 
   let fn_builder partial_expr ({ pld_name; pld_type; _ } : label_declaration) =
-    let subexpr = expr_of_typ ~true_recurse pld_type in
+    let subexpr = expr_of_typ ~substitutions pld_type in
     let var_pat = Ast_builder.Default.pvar ~loc:pld_name.loc pld_name.txt in
     (* TODO correct loc?  *)
     let str = Ast_builder.Default.estring ~loc:pld_name.loc pld_name.txt in
@@ -216,14 +214,19 @@ let implementation_generator ~(loc : location) ~rec_flag type_decl : expression
     =
   let rec_flag = really_recursive rec_flag [ type_decl ] in
   let name = to_decoder_name type_decl.ptype_name.txt in
-  let true_recurse =
+  let substitutions =
     match rec_flag with
     | Nonrecursive -> []
-    | Recursive -> [ core_type_of_type_declaration type_decl ]
+    | Recursive ->
+        [
+          ( core_type_of_type_declaration type_decl,
+            Ast_builder.Default.evar ~loc
+              (to_decoder_name type_decl.ptype_name.txt ^ "_aux") );
+        ]
   in
   let imple_expr =
     match (type_decl.ptype_kind, type_decl.ptype_manifest) with
-    | Ptype_abstract, Some manifest -> expr_of_typ ~true_recurse manifest
+    | Ptype_abstract, Some manifest -> expr_of_typ ~substitutions manifest
     | Ptype_variant cstrs, None ->
         let constr_decs =
           Ast_builder.Default.(
@@ -252,7 +255,7 @@ let implementation_generator ~(loc : location) ~rec_flag type_decl : expression
                          s;
                          [%expr
                            D.field [%e s]
-                             [%e expr_of_constr_decl ~true_recurse cstr]];
+                             [%e expr_of_constr_decl ~substitutions cstr]];
                        ])
                  cstrs))
         in
@@ -263,7 +266,8 @@ let implementation_generator ~(loc : location) ~rec_flag type_decl : expression
         [%expr
           let open D in
           [%e full_dec]]
-    | Ptype_record label_decs, _ -> expr_of_record ~true_recurse ~loc label_decs
+    | Ptype_record label_decs, _ ->
+        expr_of_record ~substitutions ~loc label_decs
     | Ptype_open, _ -> Location.raise_errorf ~loc "Unhandled open"
     | _ -> Location.raise_errorf ~loc "Unhandled mystery"
   in
@@ -271,15 +275,20 @@ let implementation_generator ~(loc : location) ~rec_flag type_decl : expression
   | Nonrecursive -> imple_expr
   | Recursive -> wrap_as_aux ~loc ~name ~expr:imple_expr
 
-let str_gen ~(loc : location) ~rec_flag type_decl : structure_item list =
+let single_type_decoder_gen ~(loc : location) ~rec_flag type_decl :
+    structure_item list =
   let imple = implementation_generator ~loc ~rec_flag type_decl in
   let name = to_decoder_name type_decl.ptype_name.txt in
   [%str let [%p Ast_builder.Default.pvar ~loc name] = [%e imple]]
+
+(* let mutual_rec_types_decoders_gen ~(loc : location) type_decls = [] *)
 
 let str_gens ~(loc : location) ~(path : label)
     ((rec_flag : rec_flag), type_decls) : structure_item list =
   let _path = path in
   match (really_recursive rec_flag type_decls, type_decls) with
-  | Nonrecursive, _ -> List.(flatten (map (str_gen ~loc ~rec_flag) type_decls))
-  | Recursive, [ type_decl ] -> str_gen ~loc ~rec_flag type_decl
-  | Recursive, _ -> []
+  | Nonrecursive, _ ->
+      List.(flatten (map (single_type_decoder_gen ~loc ~rec_flag) type_decls))
+  | Recursive, [ type_decl ] -> single_type_decoder_gen ~loc ~rec_flag type_decl
+  | Recursive, _type_decls -> []
+(* mutual_rec_types_decoders_gen ~loc type_decls *)
