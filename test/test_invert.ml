79dd58394fe601e128fe3517f05c751a75ef7286
Ben Bellick <benbellick@protonmail.com>
2024-12-09 22:29:32 -0500

    good invert test suite

--- test/test_invert.ml
+++ test/test_invert.ml
@@ -10,12 +10,13 @@ type expr =
   | Div of expr * expr
 [@@deriving decoders, encoders]
 
-type my_list = Null | L of my_list [@@deriving decoders]
-
-let expr_id =
-  let str_enc = E.encode_string expr_encoder in
-  let str_dec = D.decode_string expr_decoder in
+let make_id enc dec =
+  let str_enc = E.encode_string enc in
+  let str_dec = D.decode_string dec in
   CCFun.(str_dec % str_enc)
+
+let check id v = match id v with Ok v' when v = v' -> true | _ -> false
+let expr_id = make_id expr_encoder expr_decoder
 
 let expr_id_print v =
   let str_enc = E.encode_string expr_encoder in
@@ -24,11 +25,43 @@ let expr_id_print v =
   print_endline s;
   str_dec s
 
-let check id v = match id v with Ok v' when v = v' -> true | _ -> false
-let%test "expr_inv1" = check expr_id (Int 10)
+let%test "expr_inv:1" = check expr_id (Int 10)
 
-let%test "expr_inv2" =
+let%test "expr_inv:2" =
   check expr_id
     (Add
        ( Add (Int 10, Int (-5)),
          Div (Real 1.4929, Sub (Real (-5392.1239230), Int 58292349823)) ))
+
+type my_list = Null | L of my_list [@@deriving decoders, encoders]
+
+let my_list_id = make_id my_list_encoder my_list_decoder
+let%test "my_list:1" = check my_list_id Null
+let%test "my_list:2" = check my_list_id (L (L (L (L Null))))
+
+type a_rec = { b : b_rec option }
+and b_rec = { a : a_rec option } [@@deriving decoders, encoders]
+
+let a_rec_id = make_id a_rec_encoder a_rec_decoder
+let%test "a_rec:1" = check a_rec_id { b = None }
+let%test "a_rec:2" = check a_rec_id { b = Some { a = None } }
+
+let%test "a_rec:3" =
+  check a_rec_id { b = Some { a = Some { b = Some { a = None } } } }
+
+(* More complex mutual recursive type *)
+type a1 = { l : b1 option; m : c1 option }
+and b1 = { n : c1 }
+and c1 = { o : a1 } [@@deriving decoders, encoders]
+
+let a1_id = make_id a1_encoder a1_decoder
+let%test "a1:1" = check a1_id { l = None; m = None }
+
+let%test "a1:2" =
+  check a1_id
+    {
+      l =
+        Some
+          { n = { o = { l = None; m = Some { o = { l = None; m = None } } } } };
+      m = None;
+    }
